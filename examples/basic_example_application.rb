7af4ffc81a06f0dac82c709372e610675dcd2d19
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2008-12-12 03:39:13 +0100

    Don't use String bodies in the example application

--- examples/basic_example_application.rb
+++ examples/basic_example_application.rb
@@ -24,11 +24,11 @@ class BasicExampleApplication
       <esi:include src="/header"/>
       <p>Welcome!</p>
     }.gsub(/^\s*/, "").strip
-    [200, {"Content-Type" => "text/html"}, body]
+    [200, {"Content-Type" => "text/html"}, [body]]
   end
   
   def header(env)
     body = %{<p>#{Time.now} &ndash; You're not logged in. <a href="#">Click here to login!</a></p>}
-    [200, {"Content-Type" => "text/html"}, body]
+    [200, {"Content-Type" => "text/html"}, [body]]
   end
 end
