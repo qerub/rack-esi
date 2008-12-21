30b3168ac6c60ffafcf4ffd786f8695926b49adf
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2008-12-21 01:50:36 +0100

    Some tweaks...

--- lib/rack/esi.rb
+++ lib/rack/esi.rb
@@ -12,7 +12,6 @@ class Rack::ESI
   def call(env)
     status, headers, unprocessed_body = original_response = @app.call(env)
 
-    return original_response unless status.to_i == 200
     return original_response unless headers["Content-Type"].to_s.match(/(ht|x)ml/) # FIXME: Use another pattern
 
     body = process_body(unprocessed_body)
@@ -23,13 +22,13 @@ class Rack::ESI
 
     xml.search("esi:include") do |include_element|
       if src = include_element["src"]
-        path_info = src                                  # TODO: Rewrite the URL to allow more than absolute paths
-        inclusion_env = env.merge("PATH_INFO" => src)    # TODO: Do something with SCRIPT_NAME/REQUEST_PATH/REQUEST_URI
-        data = process_body(@app.call(inclusion_env)[2]) # FIXME: Check the status
+        path_info = src                                     # TODO: Rewrite the URL to allow more than absolute paths
+        inclusion_env = env.merge("PATH_INFO" => path_info) # TODO: Do something with SCRIPT_NAME/REQUEST_PATH/REQUEST_URI
+        data = process_body(@app.call(inclusion_env)[2])    # FIXME: Check the status
         new_element = Hpricot::Text.new(data)
         include_element.parent.replace_child(include_element, new_element)
       else
-        raise XMLIncluder::Error, "<esi:include .../> element without @src"
+        raise Error, "<esi:include .../> element without @src"
       end
     end
 
@@ -44,8 +43,8 @@ class Rack::ESI
   private
   
   def process_body(unprocessed_body)
-    body = ""
-    unprocessed_body.each { |part| body << part }
-    return body
+    parts = []
+    unprocessed_body.each { |part| parts << part }
+    return parts.join("")
   end
 end
