b77d745b63000c3ee705d7455004cdd80e36bbd7
Christoffer Sawicki <christoffer.sawicki@gmail.com>
2009-12-24 02:48:13 +0100

    Beef up example application

--- examples/basic_example_application.rb
+++ examples/basic_example_application.rb
@@ -1,36 +1,63 @@
 require "rack"
 
 class BasicExampleApplication
-  def initialize
-    @map = initialize_map
-  end
-
   def call(env)
-    @map.call(env)
+    request  = Rack::Request.new(env)
+    response = Rack::Response.new
+    
+    if request.path_info == "/"
+      action = "index"
+    else
+      action = request.path_info[1..-1]
+    end
+    
+    if ACTIONS.include?(action)
+      send(action, request, response)
+    else
+      response.status = 404
+      response.write("404 Not Found")
+    end
+    
+    response.finish
   end
 
   private
+  
+  ACTIONS = %<index header login logout>
 
-  def initialize_map
-    Rack::URLMap.new({
-      "/"       => method(:index),
-      "/header" => method(:header)
-    })
-  end
-
-  def index(env)
+  def index(request, response)
     sleep(2) # A heavy computation...
 
-    body = %{
+    response.status = 200
+    response["Cache-Control"] = "public, max-age=10"
+    response.write(%{
       <title>BasicExampleApplication</title>
       <esi:include src="/header"/>
       <p>Welcome!</p>
-    }.gsub(/^\s*/, "").strip
-    [200, {"Content-Type" => "text/html", "Cache-Control" => "public, max-age=10"}, [body]]
+    }.gsub(/^\s*/, "").strip)
   end
 
-  def header(env)
-    body = %{<p>#{Time.now} &ndash; You're not logged in. <a href="#">Click here to login!</a></p>}
-    [200, {"Content-Type" => "text/html", "Cache-Control" => "private, max-age=0, must-revalidate"}, [body]]
+  def header(request, response)
+    response.status = 200
+    response["Cache-Control"] = "private, max-age=0, must-revalidate"
+
+    if request.cookies["logged_in"].to_s == "true"
+      username = request.cookies["username"]
+      response.write(%{<p>You're logged in as #{username}. <a href="/logout">Log out.</a></p>})
+    else
+      response.write(%{<p>You're not logged in. <a href="/login">Click here to log in!</a></p>})
+    end
+  end
+  
+  def login(request, response)
+    response.set_cookie("logged_in", "true")
+    response.set_cookie("username", "John Doe")
+    response.redirect("/")
+  end
+
+  def logout(request, response)
+    response.delete_cookie("logged_in")
+    response.delete_cookie("username")
+    response.redirect("/")
   end
 end
